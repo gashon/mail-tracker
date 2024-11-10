444f6eb52aea57d9322f8a7dcfd257582a9c77dc
gashon <gashon96@gmail.com>
2024-11-10 01:50:47 -0800

    feat: integrate db connection

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
 import status from 'http-status';
 
+import { PixelTrackingRepository } from '@mail/db/repositories';
 import type { EmailMetadata } from '@mail/extension/types';
 
 const router: express.IRouter = express.Router();
+
+const db = new PixelTrackingRepository();
+
+db.insert({
+  pixelId: '123',
+  subject: 'Test',
+  timestamp: Date.now(),
+  to: ['gashon@ghussein.org'],
+});
 
 router.get('/pixels/:id', (req, res) => {
   console.log('tracking pixel');
@@ -12,6 +22,7 @@ router.get('/pixels/:id', (req, res) => {
 
 router.put('/pixels', (req, res) => {
   const body = req.body as EmailMetadata;
+  db.insert(body);
   console.log('created pixel', req.body);
   res.status(status.CREATED).json({ pixelId: body.pixelId });
 });
